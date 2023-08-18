3346d0f17512494a26d1adb8e685f3ebcd67a681
Ram <Ram50848@gmail.com>
2023-08-18 14:54:07 +0530

    java program

---
+++ add.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class sum
+{
+	public static void main(String args[])
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter a value:");
+	int a=sc.nextInt();
+	System.out.println("enter b value:");
+	int b=sc.nextInt();
+	System.out.println("sum of two numbers=" + (a+b));
+	}}
